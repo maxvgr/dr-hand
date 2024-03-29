06442ff3e05fbc4d6a182b343c2ac79ab3adab19
Max V Grushin <support@ids.spb.ru>
2024-03-29 16:52:21 +0300

    виджет отзывов

--- src/js/layout/page/home.js
+++ src/js/layout/page/home.js
@@ -1,6 +1,49 @@
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
+import {
+  Navigation,
+  Autoplay,
+  Mousewheel,
+} from "swiper/modules";
+
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
+});
+
+
+new Swiper(".profile-medication .swiper", {
+  modules: [Navigation, Autoplay],
+
+  slidesPerView: 4.8,
+  spaceBetween: 20,
+  // loop: true,
+
+  speed: 2000,
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 20
+  //   },
+  //   1400: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 20
+  //   }
+  // },
+
+  navigation: {
+    nextEl: ".profile-medication .swiper-button-next",
+    prevEl: ".profile-medication .swiper-button-prev",
+  },
+
 });
