48b517e6b57b7301e0f4cebd2dc0601f74747073
Max V Grushin <support@ids.spb.ru>
2024-06-04 18:00:38 +0300

    feat: добавлена страница кейса

--- src/js/layout/page/cases.js
+++ src/js/layout/page/cases.js
@@ -53,3 +53,46 @@ if (document.querySelector('#single-cases')) {
   new Accordion('.accordion-container');
 }
 
+
+if (document.querySelector('#single-help')) {
+  new Swiper(".case .swiper", {
+    modules: [Navigation, Autoplay],
+
+    slidesPerView: 1.5,
+    spaceBetween: 20,
+    loop: true,
+
+    speed: 2000,
+
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+
+    breakpoints: {
+      540: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      1200: {
+        slidesPerView: 3.5,
+        spaceBetween: 20
+      },
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 20
+      }
+    },
+
+    navigation: {
+      nextEl: ".help .swiper-button-next",
+      prevEl: ".help .swiper-button-prev",
+    },
+
+  });
+}
+
